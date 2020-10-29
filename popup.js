11db8159fde6951ae00c5a4da2fd39600e870a83
Abakumov <Denis.Abakumov@investorsgroup.com>
2020-10-29 15:36:59 -0500

    Removed search button. Search result is updated after any input

--- popup.js
+++ popup.js
@@ -1,37 +1,40 @@
   let searchTermInput = document.getElementById('searchTermInput');
-  let filterButton = document.getElementById('filterButton');
 
   chrome.storage.sync.get('searchTerm', function(data) {
     searchTermInput.value = data.searchTerm;
     searchTermInput.setAttribute('value', data.searchTerm);
   });
   
-  filterButton.onclick = function(element) {
+  const doSearch = () => {
     let searchTerm = searchTermInput.value;
-
-	// Reset style
-	let xpath = '"//div[starts-with(@class,\'JobsList__row\') '
-			+ 'and (contains(@style, \'display\'))]"';
-	let evaluate = 'document.evaluate(' + xpath + ', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;';
-	let code = 'var element = ' + evaluate
-		        + 'while(element) {'
-				+ '  element.style.display = null;'
-				+ '  element = ' + evaluate
-				+ '}';
-
-	// Hide divs not containing the serach term
-	xpath = '"//div[starts-with(@class,\'JobsList__row\') and not (contains(@style, \'display\'))]"';
-	evaluate = 'document.evaluate(' + xpath + ', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;';
-	code += 'element = ' + evaluate
-		        + 'while(element) {'
-				+ '  if (element.getAttribute("data-aid").includes("' + searchTerm + '")) {' 
-				+ '    element.style.display = "block";'
-				+ '  } else {'
-				+ '    element.style.display = "none";'
-				+ '  }'
-				+ '  element = ' + evaluate
-				+ '}';
+    chrome.storage.sync.set({searchTerm: searchTerm}, ()=>{});  
+    // Reset style
+    let xpath = '"//div[starts-with(@class,\'JobsList__row\') '
+    		+ 'and (contains(@style, \'display\'))]"';
+    let evaluate = 'document.evaluate(' + xpath + ', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue';
+    let code = 'var element = ' + evaluate + ';'
+    			+ 'while(element) {'
+    			+ '  element.style.display = null;'
+    			+ '  element = ' + evaluate
+    			+ '}';
+    // Hide divs not containing the serach term
+    xpath = '"//div[starts-with(@class,\'JobsList__row\') and not (contains(@style, \'display\'))]"';
+    evaluate = 'document.evaluate(' + xpath + ', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue';
+    code += 'searchTerm = "' + searchTerm + '";'
+          + 'element = ' + evaluate + ';'
+          + 'while(element) {'
+          + '  if (element.getAttribute("data-aid").includes(searchTerm)) {' 
+          + '    element.style.display = "block";'
+          + '  } else {'
+          + '    element.style.display = "none";'
+          + '  }'
+          + '  element = ' + evaluate
+          + '}';
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.executeScript(tabs[0].id, {code: code});
+    	chrome.tabs.executeScript(tabs[0].id, {code: code});
     });
-  };
+  }
+  
+  searchTermInput.onkeydown = (element) => {
+    doSearch();
+  }
